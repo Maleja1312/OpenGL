154f3943dd8c487c2b5cff29a8188ecf73c707b5
Maria Alejandra Martinez Cano <est.mariaa.martinezc@unimilitar.edu.co>
2026-03-06 23:00:47 -0500

    Cambio de color del triangulo

---
+++ shader_sources.h
@@ -0,0 +1,22 @@
+#ifndef SHADER_SOURCES_H
+#define SHADER_SOURCES_H
+
+// Código fuente de los shaders separado en un archivo aparte
+static const char* vertexShaderSource = "#version 330 core\n"
+    "layout (location = 0) in vec3 aPos;\n"
+    "out vec4 vertexColor;"
+    "void main()\n"
+    "{\n"
+    "   gl_Position = vec4(aPos, 1.0);\n"
+    "   vertexColor = vec4(1.0, 0.84, 1.0, 0.0);"
+    "}\0";
+
+static const char* fragmentShaderSource = "#version 330 core\n"
+    "out vec4 FragColor;\n"
+	"in vec4 vertexColor;\n"
+    "void main()\n"
+    "{\n"
+    "   FragColor = vertexColor;\n"
+    "}\n\0";
+
+#endif // SHADER_SOURCES_H
